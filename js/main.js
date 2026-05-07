f133312533fbe85ec0ca1698304eb939e04686e7
garciamuelacolumnas-cpu <columnasopiniongarciamuela@gmail.com>
2026-05-07 12:15:02 -0600

    Primer commit BIOCIELO

---
+++ js/main.js
@@ -0,0 +1,4 @@
+function mostrarMensaje() {
+  const mensaje = document.getElementById("mensaje");
+  mensaje.textContent = "🌿 ¡Gracias por ser parte de BIOCIELO! La ciencia y la tecnología también protegen la vida silvestre.";
+}
